cc76947c07a2af870b2042f2959c21bad43fe574
mayu <mayu6v0@me.com>
2022-03-07 21:04:02 +0900

    practice

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+    <p v-if="$auth.loggedIn">Name: {{ $auth.user.name }}</p>
+    <p v-if="$auth.loggedIn">Email: {{ $auth.user.email }}</p>
+    <p v-if="$auth.loggedIn">Password: {{ $auth.user.password }}</p>
+  </div>
+</template>
